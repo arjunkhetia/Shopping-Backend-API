e8fc7fd5138993f433cc117d53dbea60261e3997
Arjun Khetia <arjunkhetia@gmail.com>
2020-05-14 16:35:39 +0530

    Shopping API Server

--- routes/body.js
+++ routes/body.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../dbconfig');
+var async = require('async');
 
 /**
  * This function comment is parsed by doctrine
@@ -9,14 +10,44 @@ var db = require('../dbconfig');
  * @param {string} lang.query.notrequired - User's Language - eg: 'en'
  * @returns {object} 200
  */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res, next) => {
+  let finalResult = [];
   let lang = req.query.lang ? req.query.lang : 'en';
-  db.get().collection('offer')
-  .find({})
-  .toArray(function(err, result) {
-      if(err) throw err;
-      result[0]['title'] = result[0]['title'][lang];
+  async.waterfall([
+    function (callback) {
+      db.get().collection('offer')
+        .find({})
+        .toArray(function (err, result) {
+          if (err) callback(err);
+          result[0]['title'] = result[0]['title'][lang];
+          finalResult.push({"offer": result});
+          callback(null, finalResult);
+        });
+    },
+    function (finalResult, callback) {
+      db.get().collection('slider')
+        .find({})
+        .toArray(function (err, result) {
+          if (err) callback(err);
+          finalResult.push({"slider": result});
+          callback(null, finalResult);
+        });
+    },
+    function (finalResult, callback) {
+      db.get().collection('advertise')
+        .find({})
+        .toArray(function (err, result) {
+          if (err) callback(err);
+          finalResult.push({"advertise": result});
+          callback(null, finalResult);
+        });
+    }
+  ], function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
       res.send(result);
+    }
   });
 });
 
